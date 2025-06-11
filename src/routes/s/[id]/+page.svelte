20998b514e856b8b2dde97e28fa11d73a71130c3
vivek-gite <vivekkumargite@outlook.com>
2025-06-12 05:14:42 +0530

    Second commit

---
+++ src/routes/s/[id]/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+  
+    import Session from "$lib/Session.svelte";
+  
+    let title: string = "Remote Terminal | tetrax";
+  </script>
+  
+  <svelte:head>
+    <title>{title}</title>
+  
+    <style>
+      body {
+        overscroll-behavior: none;
+      }
+    </style>
+  </svelte:head>
+  
+  <Session
+    id={$page.params.id}
+    on:receiveName={({ detail: sessionName }) => {
+      if (sessionName) {
+        title = `${sessionName} | tetrax`;
+      }
+    }}
+  />
+  
